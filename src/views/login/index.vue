62b8b97fb867afbc57eba109538647c0b46eb756
phoebe <506014196@qq.com>
2021-03-15 20:16:03 +0800

    完成登录及表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,32 +3,59 @@
     <!-- 导航栏 -->
     <van-nav-bar title="明日头条-登录"></van-nav-bar>
     <!-- 表单 -->
-    <van-form>
+    <van-form ref="form" :show-error-message="false">
       <van-field
         v-model="mobile"
         label="手机号"
         placeholder="请输入手机号"
+        required
+        :rules="[{required: true, validatorForMobile, message: '请输入手机号',trigger:'onBlur'}]"
       />
       <van-field
         v-model="code"
         label="密码"
         placeholder="请输入密码号"
+        required
+        :rules="[{ required: true, validatorForCode, message: '请输入验证码', trigger: 'onBlur' }]"
       />
     </van-form>
     <!-- 按钮 -->
     <div class="btn-wrap">
-      <van-button type="info" class="btn">登录</van-button>
+      <van-button type="info" class="btn" @click="hLogin">登录</van-button>
     </div>
   </div>
 </template>
 
 <script>
+import { login } from '@/api/user.js'
+
 export default {
   name: 'Login',
   data () {
     return {
       mobile: '',
       code: ''
+    }
+  },
+  methods: {
+    // 验证码校验规则
+    validatorForCode (val) {
+      return val !== ''
+    },
+    // 手机号校验规则
+    validatorForMobile (val) {
+      return val !== ''
+    },
+    // 登录
+    hLogin () {
+      this.$refs.form.validate().then(async () => {
+        const { mobile, code } = this
+        const res = await login(mobile, code)
+        console.log(res)
+      }).catch(error => {
+        console.log(error)
+        this.$toast('手机号或密码格式有误')
+      })
     }
   }
 }
