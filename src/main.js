093cb89ec579a20ecca51f197b04f03e7cea26bf
phoebe <506014196@qq.com>
2021-03-15 19:03:15 +0800

    使用rem

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 导入amfe-flexible
+import 'amfe-flexible'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
