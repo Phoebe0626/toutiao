19f4d2ec09d41cd22c154917f6aa63cdb6d3115d
phoebe <506014196@qq.com>
2021-03-15 19:09:24 +0800

    新建登录页面配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home'
+import Login from '@/views/login'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    component: Login
   }
 ]
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login_container">
+    login
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
